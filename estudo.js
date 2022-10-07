661989f4d4506a74852c086797382bb6565372a7
edu2802 <110109872+edu2802@users.noreply.github.com>
2022-10-07 12:00:01 -0300

    Create estudo.js

---
+++ estudo.js
@@ -0,0 +1,12 @@
+function total (){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r =0
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+        document.write("Mês " + i + "valor:" + r +"<br>");
+        v = r;
+    }
+    document.write("resultado: " + r);
+}
